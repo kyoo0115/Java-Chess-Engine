4e8d6259eea8d69b940e1740fff126c495b7117c
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-08-05 18:32:28 +0800

    chore: 필요하지 않는 부분 주석 처리

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,11 +1,11 @@
 package com.project;
 
-import com.project.gui.Table;
+
 import javax.swing.SwingUtilities;
 
 public class Main {
 
   public static void main(String[] args) {
-    SwingUtilities.invokeLater(Table::new);
+//    SwingUtilities.invokeLater(Table::new);
   }
 }
