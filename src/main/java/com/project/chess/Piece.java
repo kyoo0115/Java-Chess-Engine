eac30e7c4bafb4b39f656d3eb8fa7db93cee45f3
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-08-05 16:19:09 +0800

    feat: added logic if it is a first move, for now there is no logic to determine if it is first move or not

--- src/main/java/com/project/chess/Piece.java
+++ src/main/java/com/project/chess/Piece.java
@@ -6,14 +6,20 @@ public abstract class Piece {
 
   protected final int piecePosition;
   protected final Alliance pieceAlliance;
+  protected final boolean isFirstMove;
 
   protected Piece(final int piecePosition, final Alliance pieceAlliance) {
     this.piecePosition = piecePosition;
     this.pieceAlliance = pieceAlliance;
+    this.isFirstMove = false;
   }
 
   public Alliance getPieceAlliance() {
     return this.pieceAlliance;
+  }
+
+  public boolean isFirstMove() {
+    return this.isFirstMove;
   }
 
   public abstract Collection<Move> calculateLegalMoves(final Board board);
