a6f4b0f51bf47f277056fee0c86bb6b0a0f4ef85
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-08-05 16:02:24 +0800

    feat: added enum to return a 0 or 1 depending on their color

--- src/main/java/com/project/chess/Alliance.java
+++ src/main/java/com/project/chess/Alliance.java
@@ -1,6 +1,18 @@
 package com.project.chess;
 
 public enum Alliance {
-  WHITE,
-  BLACK
+  WHITE {
+    @Override
+    public int getDirection() {
+      return 0;
+    }
+  },
+  BLACK {
+    @Override
+    public int getDirection() {
+      return 1;
+    }
+  };
+
+  public abstract int getDirection();
 }
