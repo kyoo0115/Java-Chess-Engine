5247b443cbf1eee24574e1bda38f876f2c8c364e
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-08-05 16:18:49 +0800

    feat: added logic for 1 step forward and 2 steps forward depending on their location

--- src/main/java/com/project/chess/pieces/Pawn.java
+++ src/main/java/com/project/chess/pieces/Pawn.java
@@ -1,7 +1,10 @@
 package com.project.chess.pieces;
 
+import static com.project.util.BoardUtil.SECOND_ROW;
+import static com.project.util.BoardUtil.SEVENTH_ROW;
 import static com.project.util.BoardUtil.isValidCoordinate;
 
+import com.google.common.collect.ImmutableList;
 import com.project.chess.Alliance;
 import com.project.chess.Board;
 import com.project.chess.Move;
@@ -10,37 +13,59 @@ import com.project.chess.Piece;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import org.checkerframework.checker.units.qual.A;
 
+/**
+ * 체스의 폰(병사)을 나타냅니다.
+ */
 public class Pawn extends Piece {
 
-  private static final int[] CANDIDATE_MOVE_COORDINATES = {8};
+  // 폰의 가능한 이동 오프셋
+  private static final int[] CANDIDATE_MOVE_COORDINATES = {8, 16};
 
-  protected Pawn(int piecePosition, Alliance pieceAlliance) {
+  /**
+   * 폰 조각의 생성자.
+   *
+   * @param piecePosition 조각의 위치.
+   * @param pieceAlliance 조각의 연합(색깔).
+   */
+  protected Pawn(final int piecePosition, final Alliance pieceAlliance) {
     super(piecePosition, pieceAlliance);
   }
 
+  /**
+   * 주어진 보드에서 폰의 합법적인 이동을 계산합니다.
+   *
+   * @param board 이동을 계산할 보드.
+   * @return 합법적인 이동 목록.
+   */
   @Override
-  public Collection<Move> calculateLegalMoves(Board board) {
+  public Collection<Move> calculateLegalMoves(final Board board) {
 
     final List<Move> legalMoves = new ArrayList<>();
 
     for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
-
+      // 이동 후보 좌표 계산
       int candidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * currentCandidateOffset);
 
-      if(!isValidCoordinate(candidateDestinationCoordinate)) {
+      if (!isValidCoordinate(candidateDestinationCoordinate)) {
         continue;
       }
 
-      if(currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
-        //todo
-        if(!board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+      // 한 칸 앞으로 이동
+      if (currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+      }
+      // 두 칸 앞으로 이동
+      else if (currentCandidateOffset == 16 && this.isFirstMove() &&
+          ((SECOND_ROW[this.piecePosition] && this.pieceAlliance.isBlack()) ||
+              (SEVENTH_ROW[this.piecePosition] && this.pieceAlliance.isWhite()))) {
+        final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+        if (!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+            !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
           legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
         }
       }
-
-
     }
+    return ImmutableList.copyOf(legalMoves);
   }
 }
