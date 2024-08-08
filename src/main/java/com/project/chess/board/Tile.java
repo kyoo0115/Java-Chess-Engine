c30822bef019c1fd43c5053c2f97eabd7f1dfd26
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-08-08 20:31:47 +0800

    feat: 체스보드 타일 관련 로직 기능 구현

--- src/main/java/com/project/chess/board/Tile.java
+++ src/main/java/com/project/chess/board/Tile.java
@@ -1,54 +1,55 @@
-package com.project.chess;
+package com.project.chess.board;
 
 import com.google.common.collect.ImmutableMap;
-import com.project.util.BoardUtil;
+import com.project.chess.pieces.Piece;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.Getter;
 
+/**
+ * 체스 보드의 타일을 나타내는 추상 클래스입니다.
+ */
+@Getter
 public abstract class Tile {
 
-  protected final int coordinate;
-
+  protected final int tileCoordinate;
   private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
   /**
-   * Creates a map of all possible empty tiles.
+   * 모든 가능한 빈 타일의 맵을 생성합니다.
    *
-   * @return an immutable map of empty tiles.
+   * @return 빈 타일의 불변 맵
    */
   private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
     final Map<Integer, EmptyTile> emptyTiles = new HashMap<>();
-
     for (int i = 0; i < BoardUtil.NUM_TILES; i++) {
       emptyTiles.put(i, new EmptyTile(i));
     }
-
     return ImmutableMap.copyOf(emptyTiles);
   }
 
   /**
-   * Factory method to create a Tile.
+   * 타일을 생성하는 팩토리 메서드입니다.
    *
-   * @param coordinate the coordinate of the tile.
-   * @param piece      the piece on the tile, if any.
-   * @return an OccupiedTile if a piece is provided, otherwise an EmptyTile.
+   * @param coordinate 타일의 좌표
+   * @param piece      타일에 있는 조각, 없을 경우 null
+   * @return 조각이 있으면 OccupiedTile, 없으면 EmptyTile을 반환
    */
   public static Tile createTile(final int coordinate, final Piece piece) {
     return piece != null ? new OccupiedTile(coordinate, piece) : EMPTY_TILES_CACHE.get(coordinate);
   }
 
-  private Tile(final int coordinate) {
-    this.coordinate = coordinate;
-  }
-
-  public int getCoordinate() {
-    return coordinate;
+  private Tile(final int tileCoordinate) {
+    this.tileCoordinate = tileCoordinate;
   }
 
   public abstract boolean isTileOccupied();
 
   public abstract Piece getPiece();
 
+  /**
+   * 빈 타일을 나타내는 클래스입니다.
+   */
   public static final class EmptyTile extends Tile {
 
     EmptyTile(final int coordinate) {
@@ -64,8 +65,16 @@ public abstract class Tile {
     public Piece getPiece() {
       return null;
     }
+
+    @Override
+    public String toString() {
+      return "-";
+    }
   }
 
+  /**
+   * 조각이 있는 타일을 나타내는 클래스입니다.
+   */
   public static final class OccupiedTile extends Tile {
 
     private final Piece pieceOnTile;
@@ -83,6 +92,12 @@ public abstract class Tile {
     @Override
     public Piece getPiece() {
       return pieceOnTile;
+    }
+
+    @Override
+    public String toString() {
+      return getPiece().getPieceAlliance().isBlack() ? getPiece().toString().toLowerCase()
+          : getPiece().toString();
     }
   }
 }
